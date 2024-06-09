a99e1b05b4e2bb138563311c61342e8eb12c4c4a
GeonH0 <h2jo369@naver.com>
2024-06-09 23:27:41 +0900

    Feat: Recipe,RecipeType 엔티티 정의

---
+++ HomeCafeRecipes/HomeCafeRecipes/Domain/Entities/Recipe.swift
@@ -0,0 +1,20 @@
+//
+//  Recipe.swift
+//  HomeCafeRecipes
+//
+//  Created by 김건호 on 6/9/24.
+//
+
+import Foundation
+
+struct Recipe {
+    let id: Int
+    let type: RecipeType
+    let name: String
+    let description: String
+    let writer: User
+    let imageUrls: [String]
+    let isLiked: Bool
+    let likeCount: Int
+    let createdAt: Date
+}
---
+++ HomeCafeRecipes/HomeCafeRecipes/Domain/Entities/RecipeType.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeType.swift
+//  HomeCafeRecipes
+//
+//  Created by 김건호 on 6/9/24.
+//
+
+
+
+enum RecipeType: String {
+    case coffee
+    case dessert
+}

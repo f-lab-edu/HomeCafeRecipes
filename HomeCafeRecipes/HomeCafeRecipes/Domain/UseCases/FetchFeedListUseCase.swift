52c981f357c222ee1e91dd4f68b0ff5158861b2c
GeonH0 <h2jo369@naver.com>
2024-06-10 16:05:06 +0900

    Feat: FetchFeedListUseCase,SearchFeedListUseCase 정의

---
+++ HomeCafeRecipes/HomeCafeRecipes/Domain/UseCases/FetchFeedListUseCase.swift
@@ -0,0 +1,30 @@
+//
+//  FetchFeedListUseCase.swift
+//  HomeCafeRecipes
+//
+//  Created by 김건호 on 6/9/24.
+//
+
+import RxSwift
+
+protocol FetchFeedListUseCase {
+    func execute() -> Observable<Result<[Recipe], Error>>
+}
+
+class DefaultFetchFeedListUseCase: FetchFeedListUseCase {
+    private let repository: FeedListRepository
+    
+    init(repository: FeedListRepository) {
+        self.repository = repository
+    }
+    
+    func execute() -> Observable<Result<[Recipe], Error>> {
+        return repository.fetchRecipes()
+            .map { recipes in
+                return .success(recipes)
+            }
+            .catch { error in
+                return .just(.failure(error))
+            }
+    }
+}
---
+++ HomeCafeRecipes/HomeCafeRecipes/Domain/UseCases/SearchFeedUseCase.swift
@@ -0,0 +1,30 @@
+//
+//  SearchFeedUseCase.swift
+//  HomeCafeRecipes
+//
+//  Created by 김건호 on 6/9/24.
+//
+
+import RxSwift
+
+protocol SearchFeedListUseCase {
+    func execute(title: String) -> Observable<Result<[Recipe], Error>>
+}
+
+class DefaultSearchFeedListUseCase: SearchFeedListUseCase {
+    private let repository: SearchFeedListRepository
+
+    init(repository: SearchFeedListRepository) {
+        self.repository = repository
+    }
+
+    func execute(title: String) -> Observable<Result<[Recipe], Error>> {
+        return repository.searchRecipes(title: title)
+            .map { recipes in
+                return .success(recipes)
+            }
+            .catch { error in
+                return .just(.failure(error))
+            }
+    }
+}

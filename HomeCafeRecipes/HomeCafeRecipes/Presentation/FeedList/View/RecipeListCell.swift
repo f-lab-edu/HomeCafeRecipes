69b6ba03ca3f8bbae678f97d0b556d5ed14317f1
GeonH0 <h2jo369@naver.com>
2024-06-19 22:51:09 +0900

    Feat: RecipeListCell 정의

---
+++ HomeCafeRecipes/HomeCafeRecipes/Presentation/FeedList/View/RecipeListCell.swift
@@ -0,0 +1,68 @@
+//
+//  RecipeListViewCell.swift
+//  HomeCafeRecipes
+//
+//  Created by 김건호 on 6/12/24.
+//
+
+import UIKit
+
+class RecipeListViewCell: UICollectionViewCell {
+    
+    private let imageView = UIImageView()
+    private let titleLabel = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupUI() {
+        contentView.addSubview(imageView)
+        contentView.addSubview(titleLabel)
+
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            imageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.75),
+            
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
+            titleLabel.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.15)
+        ])
+        
+        titleLabel.font = .systemFont(ofSize: 16, weight: .bold)
+        titleLabel.textAlignment = .center
+    }
+
+    func configure(with viewModel: RecipeListItemViewModel) {
+        titleLabel.text = viewModel.name
+        if let imageUrl = viewModel.imageURL {
+            loadImage(from: imageUrl)
+        } else {
+            imageView.image = nil
+        }
+
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+    }
+
+    private func loadImage(from url: URL) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            DispatchQueue.main.async {
+                self.imageView.image = UIImage(data: data)
+            }
+        }.resume()
+    }
+}

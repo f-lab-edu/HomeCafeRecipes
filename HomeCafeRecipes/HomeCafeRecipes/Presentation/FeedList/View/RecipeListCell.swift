db38695300ddac8eaf2a685d2d8f2813b7680a76
GeonH0 <h2jo369@naver.com>
2024-06-24 22:46:08 +0900

    Fix: RecipeListViewCell에 final 추가

--- HomeCafeRecipes/HomeCafeRecipes/Presentation/FeedList/View/RecipeListCell.swift
+++ HomeCafeRecipes/HomeCafeRecipes/Presentation/FeedList/View/RecipeListCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class RecipeListViewCell: UICollectionViewCell {
+final class RecipeListViewCell: UICollectionViewCell {
     
     private let imageView = UIImageView()
     private let titleLabel = UILabel()

34985f71326aa83e6b34cdfde7152aa47c4d13f5
GeonH0 <h2jo369@naver.com>
2024-06-25 13:12:59 +0900

    Feat: Fonts 객체 정의

---
+++ HomeCafeRecipes/HomeCafeRecipes/Resources/Fonts.swift
@@ -0,0 +1,13 @@
+//
+//  Fonts.swift
+//  HomeCafeRecipes
+//
+//  Created by 김건호 on 6/25/24.
+//
+
+import UIKit
+
+struct Fonts {
+    static let titleFont: UIFont = .systemFont(ofSize: 16, weight: .bold)
+    static let bodyFont: UIFont = .systemFont(ofSize: 14, weight: .regular)
+}

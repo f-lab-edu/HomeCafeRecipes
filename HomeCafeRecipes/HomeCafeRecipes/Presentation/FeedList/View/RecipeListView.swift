c1bc9a0f2ccf52ebf8fd57238f68d9c80cab8c5c
GeonH0 <h2jo369@naver.com>
2024-07-01 12:12:29 +0900

    Fix: RecipeListView에 UICollectionViewDataSource,UICollectionViewDelegate 정의

--- HomeCafeRecipes/HomeCafeRecipes/Presentation/FeedList/View/RecipeListView.swift
+++ HomeCafeRecipes/HomeCafeRecipes/Presentation/FeedList/View/RecipeListView.swift
@@ -50,14 +50,46 @@ final class RecipeListView: UIView {
         layout.minimumLineSpacing = Metric.minimumLineSpacing
         layout.minimumInteritemSpacing = Metric.minimumInteritemSpacing
         collectionView.collectionViewLayout = layout
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
-    func setCollectionViewDataSourceDelegate<D: UICollectionViewDataSource & UICollectionViewDelegate>(_ dataSourceDelegate: D) {
-        collectionView.dataSource = dataSourceDelegate
-        collectionView.delegate = dataSourceDelegate
-    }
-    
-    func reloadCollectionViewData() {
+    func setRecipes(_ recipes: [RecipeListItemViewModel]) {
+        self.recipes = recipes
         collectionView.reloadData()
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension RecipeListView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return recipes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecipeCell", for: indexPath) as! RecipeListViewCell
+        let recipeViewModel = recipes[indexPath.item]
+        cell.configure(with: recipeViewModel)
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension RecipeListView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let recipeListItemViewModel = recipes[indexPath.item]
+        delegate?.didSelectItem(ID: recipeListItemViewModel.id)
+    }
+}
+
+extension RecipeListView: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.size.height
+        
+        if offsetY > contentHeight - height {
+            delegate?.ScrollToBottom()
+        }
     }
 }

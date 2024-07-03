4b09f04e11fcc086f724f2b51be25ccb7e41a77f
GeonH0 <h2jo369@naver.com>
2024-07-03 21:43:19 +0900

    Feat: RecipeDetailRepository 정의

---
+++ HomeCafeRecipes/HomeCafeRecipes/Data/Repositories/RecipeDetailRepository.swift
@@ -0,0 +1,25 @@
+//
+//  RecipeDetailRepository.swift
+//  HomeCafeRecipes
+//
+//  Created by 김건호 on 6/26/24.
+//
+
+import Foundation
+import RxSwift
+
+protocol RecipeDetailRepository {
+    func fetchRecipeDetail(recipeID: Int) -> Single<Recipe>
+}
+
+class FeedListRepositoryImpl: RecipeDetailRepository {
+    private let networkService: RecipeDetailFetchService
+    
+    init(networkService: RecipeDetailFetchService) {
+        self.networkService = networkService
+    }
+    
+    func fetchRecipeDetail(recipeID: Int) -> Single<Recipe> {
+        return networkService.fetchRecipeDetail(recipeId: recipeID)
+    }
+}

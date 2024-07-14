06a6f896495eae6c7e50707cafc4df7e26e975d8
GeonH0 <h2jo369@naver.com>
2024-07-15 00:56:58 +0900

    Fix: RecipeDetailInteractor의 Ouput 제거 및 네이밍 수정

--- HomeCafeRecipes/HomeCafeRecipes/Presentation/Feed/View/RecipeDetailViewController.swift
+++ HomeCafeRecipes/HomeCafeRecipes/Presentation/Feed/View/RecipeDetailViewController.swift
@@ -13,12 +13,12 @@ final class RecipeDetailViewController: UIViewController {
     
     private let contentView = RecipeDetailView()
     private let customNavigationBar = CustomNavigationBar()
-    private let interactor: RecipeDetailInteractor
+    private let interactor: RecipeDetailInteractorImpl
     private let disposeBag = DisposeBag()
     private var recipeDetailViewModel: RecipeDetailViewModel?
     private let recipeListMapper = RecipeListMapper()
     
-    init(interactor: RecipeDetailInteractor) {
+    init(interactor: RecipeDetailInteractorImpl) {
         self.interactor = interactor
         super.init(nibName: nil, bundle: nil)
         self.interactor.setDelegate(self)
@@ -37,7 +37,7 @@ final class RecipeDetailViewController: UIViewController {
         interactor.viewDidLoad()
         contentView.customNavigationBar.backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
     }
-            
+    
     private func displayError(_ error: Error) {
         let alert = UIAlertController(title: "해당 레시피를 로드하는데 실패했습니다.", message: error.localizedDescription, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default))
@@ -60,7 +60,15 @@ extension RecipeDetailViewController: RecipeDetailInteractorDelegate {
                 self.contentView.configure(with: recipeItemViewModel)
             }
         case .failure(let error):
-            self.displayError(error)
+            DispatchQueue.main.async {
+                self.displayError(error)
+            }
         }
+    }
+}
+
+extension RecipeDetailViewController: Drawable {
+    var viewController: UIViewController? {
+        return self
     }
 }

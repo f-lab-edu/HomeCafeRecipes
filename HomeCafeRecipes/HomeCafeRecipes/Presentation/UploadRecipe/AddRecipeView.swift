e7381ee4628e15415a0e33ed7db153973c1b7cdf
GeonH0 <h2jo369@naver.com>
2024-07-18 23:25:51 +0900

    Fix: touchUpInside 줄바꿈 수정

--- HomeCafeRecipes/HomeCafeRecipes/Presentation/UploadRecipe/AddRecipeView.swift
+++ HomeCafeRecipes/HomeCafeRecipes/Presentation/UploadRecipe/AddRecipeView.swift
@@ -66,7 +66,8 @@ final class AddRecipeView: UIView {
                 handler: { [weak self] _ in
                     self?.delegate?.didTapSubmitButton()
                 }),
-            for: .touchUpInside)
+            for: .touchUpInside
+        )
         return button
     }()
     

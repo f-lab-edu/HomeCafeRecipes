dca79e93d2d797f0cf3e4c36ab5672eee787e632
GeonH0 <h2jo369@naver.com>
2024-08-14 21:23:15 +0900

    Fix: areRecipesEqual 메서드 TestUtils 분리 후 적용

--- HomeCafeRecipes/HomeCafeRecipesTests/FetchFeedListUseCaseTests.swift
+++ HomeCafeRecipes/HomeCafeRecipesTests/FetchFeedListUseCaseTests.swift
@@ -37,24 +37,6 @@ final class FetchFeedListUseCaseTests: XCTestCase {
         fetchRecipeListRepository = .init()
         disposeBag = .init()
     }
-    
-    private func areRecipesEqual(_ lhs: [Recipe], _ rhs: [Recipe]) -> Bool {
-        guard lhs.count == rhs.count else { return false }
-        for (index, recipe) in lhs.enumerated() {
-            if recipe.id != rhs[index].id ||
-                recipe.type != rhs[index].type ||
-                recipe.name != rhs[index].name ||
-                recipe.description != rhs[index].description ||
-                recipe.writer != rhs[index].writer ||
-                recipe.imageUrls != rhs[index].imageUrls ||
-                recipe.isLikedByCurrentUser != rhs[index].isLikedByCurrentUser ||
-                recipe.likeCount != rhs[index].likeCount ||
-                recipe.createdAt != rhs[index].createdAt {
-                return false
-            }
-        }
-        return true
-    }
 }
 
 extension FetchFeedListUseCaseTests {
@@ -87,7 +69,7 @@ extension FetchFeedListUseCaseTests {
         usecase.execute(pageNumber: 0)
             .subscribe(onSuccess:{ result in
                 if case .success(let fetchedRecipes) = result {
-                    XCTAssertTrue(self.areRecipesEqual(fetchedRecipes, recipe))
+                    XCTAssertTrue(TestUtils.areRecipesEqual(fetchedRecipes, recipe))
                     expectation.fulfill()
                 } else {
                     XCTFail("Expected success but got failure")

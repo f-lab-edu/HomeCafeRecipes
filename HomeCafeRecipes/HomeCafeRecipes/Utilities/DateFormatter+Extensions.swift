8148c98298ae244d78c2445d9c017ead0189ba0c
GeonH0 <h2jo369@naver.com>
2024-08-26 01:29:25 +0900

    Fix: 누락된 Utilities 추가

---
+++ HomeCafeRecipes/HomeCafeRecipes/Utilities/DateFormatter+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  DateFormatter+Extensions.swift
+//  HomeCafeRecipes
+//
+//  Created by 김건호 on 6/10/24.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static let iso8601: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }()
+}

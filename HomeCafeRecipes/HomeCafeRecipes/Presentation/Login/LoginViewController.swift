6a73bb0c34be9738f196e1d25482473533a0c310
GeonH0 <h2jo369@naver.com>
2024-08-31 22:49:10 +0900

    Feat: 로그인 클릭시 로그인 로직 연결 및 실패시 LoginError를 통해 실패 처리 추가

--- HomeCafeRecipes/HomeCafeRecipes/Presentation/Login/LoginViewController.swift
+++ HomeCafeRecipes/HomeCafeRecipes/Presentation/Login/LoginViewController.swift
@@ -7,11 +7,23 @@
 
 import UIKit
 
+import RxSwift
+
 final class LoginViewController: UIViewController {
     
     private let contentView =  LoginView()
+    private let loginInteractor: LoginInteractor
+    private let disposeBag = DisposeBag()
     private var loginViewModel: LoginViewModel?
     
+    init(loginInteractor: LoginInteractor) {
+        self.loginInteractor = loginInteractor
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func loadView() {
         view = contentView
@@ -21,6 +33,7 @@ final class LoginViewController: UIViewController {
         super.viewDidLoad()
         contentView.delegate = self
         setupUI()
+        loginInteractor.loadUser()
     }
     
     private func setupUI() {
@@ -33,6 +46,40 @@ final class LoginViewController: UIViewController {
         ])
     }
     
+    private func showCompletedAlert(title: String, message: String, success: Bool) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let confirmAction = UIAlertAction(title: "확인", style: .default) { _ in
+            if success {
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
+        alert.addAction(confirmAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func login(ID: String, password: String) {
+        loginInteractor.login(userID: ID, password: password)
+            .subscribe(onSuccess: { [weak self] result in
+                switch result {
+                case .success:
+                    print("LOGINSuccess")
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        self?.showCompletedAlert(
+                            title: error.title,
+                            message: error.localizedDescription,
+                            success: false
+                        )
+                    }
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+}
 
 extension LoginViewController: LoginViewDelegate {
     func didtapLoginButton() {

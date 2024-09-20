510697291cd39bc39ede3847e28d18283087d032
GeonH0 <h2jo369@naver.com>
2024-09-20 21:59:18 +0900

    Fix: 라우터 설정 수정

--- HomeCafeRecipes/HomeCafeRecipes/Router/Router.swift
+++ HomeCafeRecipes/HomeCafeRecipes/Router/Router.swift
@@ -125,7 +125,24 @@ extension Router {
     }
     
     func makeSignUpViewController() -> SignUpViewController {
-        let signUpViewcontroller = SignUpViewController()
+        let signUpInteractor = SignUpInteractorImpl(
+            usecase: SignUpUseCaseImpl(
+                repository: SignUpRepositoryImpl(
+                    SignUpService: SignUpServiceImpl(
+                        networkService: BaseNetworkService()
+                    )
+                )
+            ), checkeEmailUsecase: CheckEmailUseCaseImpl(
+                repository: CheckEmailRepositoryImpl(
+                    signUpPostService: SignUpServiceImpl(
+                        networkService: BaseNetworkService()
+                    )
+                )
+            )
+        )
+        let loginRouter = LoginRouterImpl(router: self)
+        let signUpViewcontroller = SignUpViewController(
+            signUpInteractor: signUpInteractor, router: loginRouter)
         return signUpViewcontroller
     }
 }
